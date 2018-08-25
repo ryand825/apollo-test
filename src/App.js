1f1711171ed53263fdca060837e8314782af457c
ryand88 <ryand8@hotmail.com>
2018-08-25 16:28:29 -0600

    another commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React, { Component } from "react";
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Post from "./Posts/Post";
 import Posts from "./Posts/Posts";
 import NewPost from "./Posts/NewPost";
 import "./App.css";
-
-const defaultState = {
-  isEditMode: false
-};
-
-const client = new ApolloClient({
-  uri: "https://api-uswest.graphcms.com/v1/cjl7c5z84014701e3zhxzy3yd/master",
-  clientState: {
-    defaults: defaultState,
-    resolvers: {}
-  }
-});
 
 // JS Query
 // client
@@ -29,24 +15,22 @@ const client = new ApolloClient({
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Router>
-          <div className="App">
-            <header className="App-header">
-              <Link to="/">
-                <h1 className="App-title">GraphQL</h1>
-              </Link>
-            </header>
-            <main>
-              <Switch>
-                <Route exact path="/" component={Posts} />
-                <Route exact path="/post/new" component={NewPost} />
-                <Route path="/post/:id" component={Post} />
-              </Switch>
-            </main>
-          </div>
-        </Router>
-      </ApolloProvider>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">
+              <h1 className="App-title">GraphQL</h1>
+            </Link>
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Posts} />
+              <Route exact path="/post/new" component={NewPost} />
+              <Route path="/post/:id" component={Post} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const defaultState = {
+  isEditMode: false
+};
+
+const cache = new InMemoryCache();
+
+persistCache({
+  // save cache to loacl storage
+  cache,
+  storage: window.localStorage
+}).then(() => {
+  // load cache from local storage, then load the app
+
+  const client = new ApolloClient({
+    cache, // persisted cache
+    uri: "https://api-uswest.graphcms.com/v1/cjl7c5z84014701e3zhxzy3yd/master",
+    clientState: {
+      defaults: defaultState,
+      resolvers: {}
+    }
+  });
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById("root")
+  );
+  registerServiceWorker();
+});
